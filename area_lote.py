1217c0c4acda39c1e265930c348598cee58a2e2e
Jessica-Ruiz <ruizrinconjessicajineth@gmail.com>
2024-08-25 21:21:25 -0500

    Hallar area

---
+++ area_lote.py
@@ -0,0 +1,6 @@
+#Calcular es area de un lote de forma triangular
+base=float(input("Ingrese la medida de la base: "))
+altura=float(input("Ingrese la medida de la altura: "))
+#formula
+area=(base*altura)/2
+print(f"El area del lote es: {area} metros cuadrados")
