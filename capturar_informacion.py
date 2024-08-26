a606d052ab004c0f3fcb4d6fa42f3eeabcf9f5d4
Jessica-Ruiz <ruizrinconjessicajineth@gmail.com>
2024-08-25 21:04:08 -0500

    Update capturar_informacion.py

--- capturar_informacion.py
+++ capturar_informacion.py
@@ -3,3 +3,4 @@ cedula=int(input("Digite su numero de cedula: "))
 estatura=float(input("Dif¿gite su estatura: "))
 estado=bool(input("Digite su estado: "))
 print(f"Su nombre es {nombre}, su cedula es {cedula}, su estatura es {estatura},su estado es {bool}")
+#Actualizar github
