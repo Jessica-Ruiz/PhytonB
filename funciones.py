f375c990e2108ec2479dad21c9530d1730c660e5
Jessica-Ruiz <ruizrinconjessicajineth@gmail.com>
2024-10-18 15:35:15 -0500

    funciones

--- funciones.py
+++ funciones.py
@@ -7,5 +7,44 @@ mostrar mensaje programacion 1:  str
     x=10
 #Llamar funcion
 mensaje()
-mensaje()
-print(x)
+
+#Crear Funcion con parametros
+def suma (num1,num2):
+    """
+    Genera la suma de dos numeros enteros 
+    con variables de entrada num1, num2
+    """
+    rta = num1 + num2 
+    print(f"La suma entre {num1} + {num2} = {rta} ")
+
+#Llamar funcion
+suma(1,2)
+
+#Funcion pedrir datos
+def solicitar_datos():
+    """
+    Solicita num1 y num2, transforma a tipo de dato 
+    entero y retorna la informacion
+    """
+    num1 = int(input("Digite el numero 1: "))
+    num2 = int(input("Digite el numero 2: "))
+    return num1,num2 
+
+#Llamar funcion
+a,b = solicitar_datos()
+print(f"Numeros digitados {a} y {b}")
+#llamar a suma
+suma(a,b)
+
+#Funcion con parametros y valores de retorno
+def multiplicacion (num1,num2):
+    """
+    2 numeros de tipo entero y los retorna 
+    """
+    rta = num1 * num2
+    return rta
+rta=multiplicacion(a,b)
+print(f"El resultado de la multiplicacion ={rta}")
+
+suma =(rta,a)
+
